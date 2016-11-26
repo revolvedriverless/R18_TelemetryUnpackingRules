54ea3f11b49ffdf4de69f04d03be0aa40d03d47d
Ole Ravna <olevra@stud.ntnu.no>
2016-11-27 00:47:21 +0100

    Update parsing.cs

--- parsing.cs
+++ parsing.cs
@@ -21,38 +21,37 @@ Available functions:
 {
      490 , data => {
         { "IMD_SHUTDOWN", Math.Sqrt(9) },
-     }                       
+     }
 },
 
 {
      123 , data => {
-        { "SINUS", 40*hexToFloat(data)+50 },
-     }                       
+        { "NUM", Int(data[3], data[2]) },
+     }
 },
 
 {
      100 , data => {
         { "DISTANCE", Int(data) },
-     }                       
+     }
 },
 
 {
      122 , data => {
-        { "POT", Int(data) / 12.0 },
-     }                       
+        { "POT", Uint(data, true) / 12.0 },
+     }
 },
 
 {
      137 , data => {
-        { "COSINUS", 40*hexToFloat(data) + 50 },
-     }                       
+        { "COSINUS", 40*Float(data) + 50 },
+     }
 },
 
 {
     310 , data => {
-        { "TPS_right", hexToUint(getByte(data, 0) + getByte(data, 1)) / 10.0 },
-        { "TPS_left", hexToUint(getByte(data, 2) + getByte(data, 3)) / 10.0 }
-     }        
+        { "TPS_right", Uint(data[0], data[1]) / 10.0 },
+        { "TPS_left", Uint(data[2], data[3]) / 10.0 }
+     }
 },
-
 
