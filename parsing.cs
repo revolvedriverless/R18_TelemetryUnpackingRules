43da3231cbbd051cd0039a611c172f331b6c2441
larsgus <larsgus@stud.ntnu.no>
2017-03-25 14:51:50 +0100

    Add some ECU and inverter rules
    Also removed dummy ones

--- parsing.cs
+++ parsing.cs
@@ -18,45 +18,127 @@ Available functions:
 */
 
 {
-     185 , data => {
-        { "SETPOINT_STATE_FL", Uint(data[1] },
+     0x185 , data => new Data {
+        { "Setpoint_FL_control_word", Uint(data[1]) },
+		{ "Setpoint_FL_RPM_request", Int(data[2], data[3]) },
+		{ "Setpoint_FL_positive_torque_limit", Int(data[4], data[5]) },
+		{ "Setpoint_FL_negative_torque_limit", Int(data[6], data[7]) },
+
      }
 },
 
 {
-     490 , data => {
-        { "IMD_SHUTDOWN", Math.Sqrt(9) },
+     0x186 , data => new Data {
+        { "Setpoint_FR_control_word", Uint(data[1]) },
+		{ "Setpoint_FR_RPM_request", Int(data[2], data[3]) },
+		{ "Setpoint_FR_positive_torque_limit", Int(data[4], data[5]) },
+		{ "Setpoint_FR_negative_torque_limit", Int(data[6], data[7]) },     }
+},
+
+{
+     0x189 , data => new Data {
+        { "Setpoint_RL_control_word", Uint(data[1]) },
+		{ "Setpoint_RL_RPM_request", Int(data[2], data[3]) },
+		{ "Setpoint_RL_positive_torque_limit", Int(data[4], data[5]) },
+		{ "Setpoint_RL_negative_torque_limit", Int(data[6], data[7]) },     }
+},
+
+{
+     0x18A , data => new Data {
+        { "Setpoint_RR_control_word", Uint(data[1]) },
+		{ "Setpoint_RR_RPM_request", Int(data[2], data[3]) },
+		{ "Setpoint_RR_positive_torque_limit", Int(data[4], data[5]) },
+		{ "Setpoint_RR_negative_torque_limit", Int(data[6], data[7]) },     }
+},
+
+{
+     0x284 , data => new Data {
+		{ "AMK_FL_Status_", Uint(data[1]) },
+		{ "AMK_FL_Actual_velocity", Int(data[2], data[3]) },
+		{ "AMK_FL_Torque_current", Int(data[4], data[5])*1072/16384*100 },
+		{ "AMK_FL_Magnetizing_current", Int(data[6], data[7]) },
+
      }
 },
 
 {
-     123 , data => {
-        { "NUM", Int(data[3], data[2]) },
+     0x285 , data => new Data {
+		{ "AMK_FR_Status", Uint(data[1]) },
+		{ "AMK_FR_Actual_velocity", Int(data[2], data[3]) },
+		{ "AMK_FR_Torque_current", (Int(data[4], data[5]))*1072/16384*100 },
+		{ "AMK_FR_Magnetizing_current", Int(data[6], data[7]) },
+
      }
 },
 
 {
-     100 , data => {
-        { "DISTANCE", Int(data) },
+     0x288 , data => new Data {
+		{ "AMK_RL_Status", Uint(data[1]) },
+		{ "AMK_RL_Actual_velocity", Int(data[2], data[3]) },
+		{ "AMK_RL_Torque_current", Int(data[4], data[5])*1072/16384*100 },
+		{ "AMK_RL_Magnetizing_current", Int(data[6], data[7]) },
+
      }
 },
 
 {
-     122 , data => {
-        { "POT", Uint(data, true) / 12.0 },
+     0x289 , data => new Data {
+		{ "AMK_RR_Status", Uint(data[1]) },
+		{ "AMK_RR_Actual_velocity", Int(data[2], data[3]) },
+		{ "AMK_RR_Torque_current", Int(data[4], data[5])*1072/16384*100 },
+		{ "AMK_RR_Magnetizing_current", Int(data[6], data[7]) },
+
+     }
+},
+
+
+
+{
+	0x450 , data => new Data {
+        { "Status_ECU_system_state", Uint(data[0],data[1]) },
+		{ "Status_Torque_allocation", Uint(data[2]) },
+        { "Status_Vehicle_state", Uint(data[3]) },
+	}
+
+},
+
+{
+	0x350 , data => new Data {
+        { "Dash_RTDS_requested", Uint(data[0]) },
+	}
+},
+
+{
+	0x359 , data => new Data {
+        { "Parameter_ID", Uint(data[0])},
+		{ "Parameter_value", Float(data[4],data[5],data[6],data[7])},
+	}
+},
+
+{
+	0x45E , data => new Data {
+        { "Soft_BSPD_status", Uint(data[0]) },
+	}
+},
+
+{
+	0x4C0 , data => new Data {
+        { "Alive_message", Uint(data[0]) },
+	}
+},
+
+{
+    0x310 , data => new Data {
+        { "Sensor_TPS_right", Uint(data[0], data[1]) / 10.0 },
+        { "Sensor_TPS_left", Uint(data[2], data[3]) / 10.0 }
      }
 },
 
 {
-     137 , data => {
-        { "COSINUS", 40*Float(data) + 50 },
-     }
-},
-
-{
-    310 , data => {
-        { "TPS_right", Uint(data[0], data[1]) / 10.0 },
-        { "TPS_left", Uint(data[2], data[3]) / 10.0 }
-     }
-},
-
+	0x459 , data => new Data {
+		{"Sensor_POS_AX_INS", Int(data[0], data[1]) },
+		{"Sensor_POS_AY_INS", Int(data[2], data[3]) },
+		{"Sensor_POS_VX_INS", Int(data[4], data[5]) },
+		{"Sensor_POS_VY_INS", Int(data[6], data[7]) }
+	}
+}, 
