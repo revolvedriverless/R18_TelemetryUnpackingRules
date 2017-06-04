b7f0f09ad9468dd63e31f66be393042d5f17644c
Lars Henrik Bolstad <lahebo@gmail.com>
2017-06-05 01:12:14 +0200

    Fix formatting

--- parsing.cs
+++ parsing.cs
@@ -18,83 +18,79 @@ Available functions:
 */
 
 {
-	0x185 , data => new Data {
+     0x185 , data => new Data {
 		{ "AMK_FL_Setpoint_control_word", Uint(data[1]) },
 		{ "AMK_FL_Setpoint_RPM_request", Int(data[2], data[3]) },
 		{ "AMK_FL_Setpoint_positive_torque_limit", Int(data[4], data[5]) },
-		{ "AMK_FL_Setpoint_negative_torque_limit", Int(data[6], data[7]) },
-	}
+		{ "AMK_FL_Setpoint_negative_torque_limit", Int(data[6], data[7]) }
+     }
 },
 
 {
-	0x186 , data => new Data {
+     0x186 , data => new Data {
 		{ "AMK_FR_Setpoint_control_word", Uint(data[1]) },
 		{ "AMK_FR_Setpoint_RPM_request", Int(data[2], data[3]) },
 		{ "AMK_FR_Setpoint_positive_torque_limit", Int(data[4], data[5]) },
-		{ "AMK_FR_Setpoint_negative_torque_limit", Int(data[6], data[7]) },
-	}
+		{ "AMK_FR_Setpoint_negative_torque_limit", Int(data[6], data[7]) }
+    }
 },
 
 {
-	0x189 , data => new Data {
+     0x189 , data => new Data {
 		{ "AMK_RL_Setpoint_control_word", Uint(data[1]) },
 		{ "AMK_RL_Setpoint_RPM_request", Int(data[2], data[3]) },
 		{ "AMK_RL_Setpoint_positive_torque_limit", Int(data[4], data[5]) },
-		{ "AMK_RL_Setpoint_negative_torque_limit", Int(data[6], data[7]) },
-	}
+		{ "AMK_RL_Setpoint_negative_torque_limit", Int(data[6], data[7]) }
+    }
 },
 
 {
-	0x18A , data => new Data {
+     0x18A , data => new Data {
 		{ "AMK_RR_Setpoint_control_word", Uint(data[1]) },
 		{ "AMK_RR_Setpoint_RPM_request", Int(data[2], data[3]) },
 		{ "AMK_RR_Setpoint_positive_torque_limit", Int(data[4], data[5]) },
-		{ "AMK_RR_Setpoint_negative_torque_limit", Int(data[6], data[7]) },
-	}
+		{ "AMK_RR_Setpoint_negative_torque_limit", Int(data[6], data[7]) }
+    }
 },
 
 {
-	0x284 , data => new Data {
+     0x284 , data => new Data {
 		{ "AMK_FL_Status_", Uint(data[1]) },
 		{ "AMK_FL_Actual_velocity", Int(data[2], data[3]) },
 		{ "AMK_FL_Torque_current", Int(data[4], data[5])*1072/16384*100 },
-		{ "AMK_FL_Magnetizing_current", Int(data[6], data[7]) },
-
-	}
+		{ "AMK_FL_Magnetizing_current", Int(data[6], data[7]) }
+     }
 },
 
 {
-	0x285 , data => new Data {
+     0x285 , data => new Data {
 		{ "AMK_FR_Status", Uint(data[1]) },
 		{ "AMK_FR_Actual_velocity", Int(data[2], data[3]) },
 		{ "AMK_FR_Torque_current", (Int(data[4], data[5]))*1072/16384*100 },
-		{ "AMK_FR_Magnetizing_current", Int(data[6], data[7]) },
-
-	}
+		{ "AMK_FR_Magnetizing_current", Int(data[6], data[7]) }
+     }
 },
 
 {
-	0x288 , data => new Data {
+     0x288 , data => new Data {
 		{ "AMK_RL_Status", Uint(data[1]) },
 		{ "AMK_RL_Actual_velocity", Int(data[2], data[3]) },
 		{ "AMK_RL_Torque_current", Int(data[4], data[5])*1072/16384*100 },
-		{ "AMK_RL_Magnetizing_current", Int(data[6], data[7]) },
-
-	}
+		{ "AMK_RL_Magnetizing_current", Int(data[6], data[7]) }
+     }
 },
 
 {
-	0x289 , data => new Data {
+     0x289 , data => new Data {
 		{ "AMK_RR_Status", Uint(data[1]) },
 		{ "AMK_RR_Actual_velocity", Int(data[2], data[3]) },
 		{ "AMK_RR_Torque_current", Int(data[4], data[5])*1072/16384*100 },
-		{ "AMK_RR_Magnetizing_current", Int(data[6], data[7]) },
-
-	}
+		{ "AMK_RR_Magnetizing_current", Int(data[6], data[7]) }
+     }
 },
 
 {
-	0x290, data => new Data {
+	0x290 , data => new Data {
 		{ "R17_State_speed", Int(data[2],data[3])},
 		{ "R17_State_encoder_data",Int(data[4],data[5],data[6],data[7])},
 		{ "R17_State_torque_request", Int(data[0], data[1])/100}
@@ -102,7 +98,7 @@ Available functions:
 },
 
 {
-	0x292, data => new Data {
+	0x292 , data => new Data {
 		{ "R17_State_current_q", Int(data[0],data[1])/100},
 		{ "R17_State_current_d",Int(data[2],data[3])/100},
 		{ "R17_State_current_a", Int(data[4], data[5])/100},
@@ -111,7 +107,7 @@ Available functions:
 },
 
 {
-	0x293, data => new Data {
+	0x293 , data => new Data {
 		{ "R17_STATUS_BITS_0-7",Int(data[0])},
 		{ "R17_STATUS_BITS_7-15",Int(data[1])},
 		{ "R17_STATUS_BITS_15-23",Int(data[2])},
@@ -126,11 +122,7 @@ Available functions:
 	}
 },
 
-{
-	0x350 , data => new Data {
-		{ "Dash_RTDS_requested", Uint(data[0]) },
-	}
-},
+{   0x350 , data => new Data {{"ECU_PLAY_RTDS", Uint(data[0])}}},
 
 {
 	0x359 , data => new Data {
@@ -161,11 +153,9 @@ Available functions:
 {
 	0x45E , data => new Data {
 		{ "Soft_BSPD_status", Uint(data[0]) },
-	}
 },
 
 {
 	0x4C0 , data => new Data {
 		{ "Alive_message", Uint(data[0]) },
-	}
 },
