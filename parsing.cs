4a42af767f5e64ac26fcd304474acbfeb2121bc5
larsgus <larsgus@stud.ntnu.no>
2017-06-18 14:28:11 +0200

    Add TV monitoring IDs

--- parsing.cs
+++ parsing.cs
@@ -1142,3 +1142,25 @@ Available functions:
         {"BMS_Cell_Temperature_143", Uint(data[6], data[7])*0.1}
     }
 },
+
+{
+    0x650 , data => new Data {
+        {"BMS_Cell_Temperature_140", Uint(data[0], data[1])*0.1},
+        {"BMS_Cell_Temperature_141", Uint(data[2], data[3])*0.1},
+        {"BMS_Cell_Temperature_142", Uint(data[4], data[5])*0.1},
+        {"BMS_Cell_Temperature_143", Uint(data[6], data[7])*0.1}
+    }
+	
+	
+{
+    0x653 , data => new Data {
+        {"ECU_TV_monitor_Yaw_rate_ref", Int(data[0], data[1])/1000},
+        {"ECU_TV_monitor_Mz_ref", Int(data[2], data[3])/10},
+        {"ECU_TV_monitor_Mz_reserved", Int(data[4], data[5])/10},
+        {"ECU_T_driver_torque_request", Uint(data[6])*2}
+		{"ECU_T_percentage_og_req_torque_allocated", Uint(data[6])*2}
+    }
+	   0x654 , data => new Data {
+        {"ECU_TV_monitor_Yaw_rate_ref", Int(data[0], data[1])/1000},
+    }
+	
